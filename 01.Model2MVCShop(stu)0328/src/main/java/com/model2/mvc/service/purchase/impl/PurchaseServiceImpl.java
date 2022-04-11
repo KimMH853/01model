5588e92aad429d00a2a2d4ca596a3ca008b4711b
jiho <thdwlgh44@naver.com>
2022-04-11 16:19:25 +0900

    04.11 new 에서 하는거 깜빡함

--- 01.Model2MVCShop(stu)0328/src/main/java/com/model2/mvc/service/purchase/impl/PurchaseServiceImpl.java
+++ 01.Model2MVCShop(stu)0328/src/main/java/com/model2/mvc/service/purchase/impl/PurchaseServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 
 import com.model2.mvc.common.SearchVO;
 import com.model2.mvc.service.purchase.PurchaseService;
+import com.model2.mvc.service.purchase.dao.PurchaseDAO;
 import com.model2.mvc.service.purchase.vo.PurchaseVO;
 
 public class PurchaseServiceImpl implements PurchaseService {
