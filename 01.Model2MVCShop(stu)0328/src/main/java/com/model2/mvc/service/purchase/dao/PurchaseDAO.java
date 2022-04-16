59b908a3b74907b21fa3787ca9efbfbaef3e91a3
jiho <thdwlgh44@naver.com>
2022-04-16 17:33:58 +0900

    22.04.16 파일업로드 추가

--- 01.Model2MVCShop(stu)0328/src/main/java/com/model2/mvc/service/purchase/dao/PurchaseDAO.java
+++ 01.Model2MVCShop(stu)0328/src/main/java/com/model2/mvc/service/purchase/dao/PurchaseDAO.java
@@ -66,64 +66,81 @@ public class PurchaseDAO {
 	 * return productVO; }
 	 */
 	  
-	/*
-	 * public HashMap<String,Object> getPurchaseList(SearchVO searchVO) throws
-	 * Exception {
-	 * 
-	 * Connection con = DBUtil.getConnection();
-	 * 
-	 * String sql = "select * from USERS "; if (searchVO.getSearchCondition() !=
-	 * null) { if (searchVO.getSearchCondition().equals("0")) { sql +=
-	 * " where USER_ID='" + searchVO.getSearchKeyword() + "'"; } else if
-	 * (searchVO.getSearchCondition().equals("1")) { sql += " where USER_NAME='" +
-	 * searchVO.getSearchKeyword() + "'"; } } sql += " order by USER_ID";
-	 * 
-	 * PreparedStatement stmt = con.prepareStatement( sql,
-	 * ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE); ResultSet rs
-	 * = stmt.executeQuery();
-	 * 
-	 * rs.last(); int total = rs.getRow(); System.out.println("로우의 수:" + total);
-	 * 
-	 * HashMap<String,Object> map = new HashMap<String,Object>(); map.put("count",
-	 * new Integer(total));
-	 * 
-	 * rs.absolute(searchVO.getPage() * searchVO.getPageUnit() -
-	 * searchVO.getPageUnit()+1); System.out.println("searchVO.getPage():" +
-	 * searchVO.getPage()); System.out.println("searchVO.getPageUnit():" +
-	 * searchVO.getPageUnit());
-	 * 
-	 * ArrayList<PurchaseVO> list = new ArrayList<PurchaseVO>(); if (total > 0) {
-	 * for (int i = 0; i < searchVO.getPageUnit(); i++) { PurchaseVO vo = new
-	 * PurchaseVO(); vo.setPurchaseId(rs.getString("USER_ID"));
-	 * vo.setPurchaseName(rs.getString("USER_NAME"));
-	 * vo.setPassword(rs.getString("PASSWORD")); vo.setRole(rs.getString("ROLE"));
-	 * vo.setSsn(rs.getString("SSN")); vo.setPhone(rs.getString("CELL_PHONE"));
-	 * vo.setAddr(rs.getString("ADDR")); vo.setEmail(rs.getString("EMAIL"));
-	 * vo.setRegDate(rs.getDate("REG_DATE"));
-	 * 
-	 * list.add(vo); if (!rs.next()) break; } } System.out.println("list.size() : "+
-	 * list.size()); map.put("list", list); System.out.println("map().size() : "+
-	 * map.size());
-	 * 
-	 * con.close();
-	 * 
-	 * return map; }
-	 * 
-	 * public void updatePurchase(PurchaseVO purchaseVO) throws Exception {
-	 * 
-	 * Connection con = DBUtil.getConnection();
-	 * 
-	 * String sql =
-	 * "update USERS set USER_NAME=?,CELL_PHONE=?,ADDR=?,EMAIL=? where USER_ID=?";
-	 * 
-	 * PreparedStatement stmt = con.prepareStatement(sql); stmt.setString(1,
-	 * purchaseVO.getPurchaseName()); stmt.setString(2, purchaseVO.getPhone());
-	 * stmt.setString(3, purchaseVO.getAddr()); stmt.setString(4,
-	 * purchaseVO.getEmail()); stmt.setString(5, purchaseVO.getPurchaseId());
-	 * stmt.executeUpdate();
-	 * 
-	 * con.close(); }
-	 */
+	
+	public HashMap<String, Object> getPurchaseList(SearchVO searchVO) throws Exception {
+
+		Connection con = DBUtil.getConnection();
+
+		String sql = "select * from USERS ";
+		if (searchVO.getSearchCondition() != null) {
+			if (searchVO.getSearchCondition().equals("0")) {
+				sql += " where USER_ID='" + searchVO.getSearchKeyword() + "'";
+			} else if (searchVO.getSearchCondition().equals("1")) {
+				sql += " where USER_NAME='" + searchVO.getSearchKeyword() + "'";
+			}
+		}
+		sql += " order by USER_ID";
+
+		PreparedStatement stmt = con.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE,
+				ResultSet.CONCUR_UPDATABLE);
+		ResultSet rs = stmt.executeQuery();
+
+		rs.last();
+		int total = rs.getRow();
+		System.out.println("로우의 수:" + total);
+
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("count", new Integer(total));
+
+		rs.absolute(searchVO.getPage() * searchVO.getPageUnit() - searchVO.getPageUnit() + 1);
+		System.out.println("searchVO.getPage():" + searchVO.getPage());
+		System.out.println("searchVO.getPageUnit():" + searchVO.getPageUnit());
+
+		ArrayList<PurchaseVO> list = new ArrayList<PurchaseVO>();
+		if (total > 0) {
+			for (int i = 0; i < searchVO.getPageUnit(); i++) {
+				PurchaseVO vo = new PurchaseVO();
+				vo.setPurchaseId(rs.getString("USER_ID"));
+				vo.setPurchaseName(rs.getString("USER_NAME"));
+				vo.setPassword(rs.getString("PASSWORD"));
+				vo.setRole(rs.getString("ROLE"));
+				vo.setSsn(rs.getString("SSN"));
+				vo.setPhone(rs.getString("CELL_PHONE"));
+				vo.setAddr(rs.getString("ADDR"));
+				vo.setEmail(rs.getString("EMAIL"));
+				vo.setRegDate(rs.getDate("REG_DATE"));
+
+				list.add(vo);
+				if (!rs.next())
+					break;
+			}
+		}
+		System.out.println("list.size() : " + list.size());
+		map.put("list", list);
+		System.out.println("map().size() : " + map.size());
+
+		con.close();
+
+		return map;
+	}
+
+	public void updatePurchase(PurchaseVO purchaseVO) throws Exception {
+
+		Connection con = DBUtil.getConnection();
+
+		String sql = "update USERS set USER_NAME=?,CELL_PHONE=?,ADDR=?,EMAIL=? where USER_ID=?";
+
+		PreparedStatement stmt = con.prepareStatement(sql);
+		stmt.setString(1, purchaseVO.getPurchaseName());
+		stmt.setString(2, purchaseVO.getPhone());
+		stmt.setString(3, purchaseVO.getAddr());
+		stmt.setString(4, purchaseVO.getEmail());
+		stmt.setString(5, purchaseVO.getPurchaseId());
+		stmt.executeUpdate();
+
+		con.close();
+	}
+	 
 	  
 	 
 }
